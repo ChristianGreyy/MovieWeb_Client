574420287024209a2d68273e7ef50aa0f39ada77
ChristianGreyy <truongthanhhung2k2@gmail.com>
2022-06-15 01:52:02 +0700

    Update

--- src/pages/WatchFilm/WatchFilm.jsx
+++ src/pages/WatchFilm/WatchFilm.jsx
@@ -6,25 +6,31 @@ import ReactPlayer from "react-player";
 import ReactHlsPlayer from "react-hls-player";
 import Hls from "hls.js";
 import { useParams } from "react-router-dom";
+import { movieService } from "../../services";
 
 const WatchFilm = () => {
   const { movieId } = useParams();
-  let videoSrc = "http://localhost:8080/videos/output.hls";
-  console.log(movieId);
+  let videoSrc = "";
 
   const video = useRef(null);
 
   useEffect(() => {
-    if (video.current) {
-      if (video.current.canPlayType("application/vnd.apple.mpegurl")) {
-        video.current.src = videoSrc;
-      } else if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource(videoSrc);
-        hls.attachMedia(video.current);
+    const streamming = (async () => {
+      videoSrc = await movieService.streamming(movieId);
+      videoSrc = "http://localhost:8080/videos/" + videoSrc.data;
+      console.log(videoSrc);
+
+      if (video.current) {
+        if (video.current.canPlayType("application/vnd.apple.mpegurl")) {
+          video.current.src = videoSrc;
+        } else if (Hls.isSupported()) {
+          var hls = new Hls();
+          hls.loadSource(videoSrc);
+          hls.attachMedia(video.current);
+        }
       }
-    }
-  }, []);
+    })();
+  }, [movieId]);
 
   // console.log(video.current);anh
 
@@ -32,26 +38,25 @@ const WatchFilm = () => {
     <div>
       <Header />
       <Body>
-        <div className="name">
-          <video
-            ref={video}
-            src={videoSrc}
-            id="video"
-            width="1280px"
-            height="600px"
-            autoPlay
-            controls
-          ></video>
-          {/* <ReactPlayer width="1280px" height="600px" controls url={videoSrc} /> */}
-          {/* <ReactHlsPlayer
-            src={url}
-            controls
-            hlsConfig={{
-              maxLoadingDelay: 4,
-              minAutoBitrate: 0,
-              lowLatencyMode: true,
-            }}
-          /> */}
+        <div className="video__container">
+          <div className="video__play">
+            <video
+              ref={video}
+              src={videoSrc}
+              className="video"
+              width="1200px"
+              id="video"
+              autoPlay
+              controls
+            ></video>
+          </div>
+          <div className="video__description">
+            <h4 className="video__heading">Tập phim</h4>
+            <div className="video__all">Tập 1 - Tập 16</div>
+            <div className="video__episode-container">
+              <div className="video__episode-item">Tập 1</div>
+            </div>
+          </div>
         </div>
       </Body>
     </div>
--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -4,8 +4,15 @@ const getMovieById = async (movieId) => {
   return await axios.get(`http://localhost:8080/api/movie/${movieId}`);
 };
 
+const streamming = async (movieId) => {
+  return await axios.get(
+    `http://localhost:8080/api/movie/${movieId}/streamming`
+  );
+};
+
 const authService = {
   getMovieById,
+  streamming,
 };
 
 export default authService;

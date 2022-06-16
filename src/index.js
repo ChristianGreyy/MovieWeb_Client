75a849cbd037ed330ec1e01bc163d790b1a81f66
Your Name <you@example.com>
2022-06-16 17:14:34 +0700

    Phim de cu

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { CookiesProvider } from "react-cookie";
+import 'tw-elements';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Body from "../../Components/Body";
 import Header from "../../Components/Header";
+import MovieNominations from "./MovieNominations/MovieNominations";
 const Home = () => {
   return (
     <div className="Home">
       <Header />
-      <Body></Body>
+      <Body>
+        <MovieNominations />
+      </Body>
     </div>
   );
 };

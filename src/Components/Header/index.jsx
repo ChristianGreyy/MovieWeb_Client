02615f936ea7530cc0826204e88154e355abe779
Laitungcute <laitung27122002@gmail.com>
2022-07-23 19:41:48 +0700

    Features/userUpdate

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -58,6 +58,12 @@ const Header = () => {
                 style={{ marginLeft: "6px" }}
                 className="fa-solid fa-caret-down"
               ></i>
+
+              <ul className="menu_navbar">
+                <li>Phim hành động</li>
+                <li>Phim ngôn tình</li>
+                <li>Phim kinh dị</li>
+              </ul>
             </strong>
           </li>
           <li>
@@ -67,6 +73,13 @@ const Header = () => {
                 style={{ marginLeft: "6px" }}
                 className="fa-solid fa-caret-down"
               ></i>
+
+              <ul className="menu_navbar">
+                <li>Hàn Quốc</li>
+                <li>Mỹ</li>
+                <li>Việt Nam</li>
+                <li>Nhật Bản</li>
+              </ul>
             </strong>
           </li>
           <li>
--- src/pages/User/Pay/Pay.jsx
+++ src/pages/User/Pay/Pay.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Body from '../../../Components/Body'
 import Header from '../../../Components/Header'
 import NavBar from '../NavBar/NavBar'
+import BoxPay from './BoxPay/BoxPay'
 import './Pay.scss'
 
 const Pay = () => {
@@ -13,8 +14,11 @@ const Pay = () => {
         <div className="Pay_container flex justify-between">
           <NavBar />
           <div className="Pay_container_content">
-            <div className="icon"><i class="fa-solid fa-file-circle-xmark"></i></div>
-            <h1>Không có dữ liệu!</h1>
+            {/* <div className="icon"><i class="fa-solid fa-file-circle-xmark"></i></div>
+            <h1>Không có dữ liệu!</h1> */}
+
+            <BoxPay />
+
           </div>
         </div>
       </Body>
